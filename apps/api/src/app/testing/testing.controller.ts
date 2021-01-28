1d056a8a18e5c816f8c798e2fd713f5fe3f25200
jimyan <jimyan@tencent.com>
2021-01-28 16:09:37 +0800

    fix: demo去掉无效的模块

--- apps/api/src/app/testing/testing.controller.ts
+++ apps/api/src/app/testing/testing.controller.ts
@@ -1,21 +1,26 @@
-import { Body, Controller, NotFoundException, Post } from '@nestjs/common';
+import { Body, Controller, NotFoundException, Post, Get } from '@nestjs/common';
 import { ISeedDataResponseDto, SeedDataBodyDto } from './dtos/seed-data.dto';
 import { SeedData } from './usecases/seed-data/seed-data.usecase';
 import { SeedDataCommand } from './usecases/seed-data/seed-data.command';
 
 @Controller('/testing')
 export class TestingController {
-  constructor(private seedDataUsecase: SeedData) {}
+  //   constructor(private seedDataUsecase: SeedData) {}
 
-  /**
-   * Used for seeding data for client e2e tests,
-   * Currently just creates a new user session and returns signed JWT
-   */
-  @Post('/seed')
-  async seedData(@Body() body: SeedDataBodyDto): Promise<ISeedDataResponseDto> {
-    if (process.env.NODE_ENV !== 'test') throw new NotFoundException();
-    const command = SeedDataCommand.create({});
+  //   /**
+  //    * Used for seeding data for client e2e tests,
+  //    * Currently just creates a new user session and returns signed JWT
+  //    */
+  //   @Post('/seed')
+  //   async seedData(@Body() body: SeedDataBodyDto): Promise<ISeedDataResponseDto> {
+  //     if (process.env.NODE_ENV !== 'test') throw new NotFoundException();
+  //     const command = SeedDataCommand.create({});
 
-    return await this.seedDataUsecase.execute(command);
+  //     return await this.seedDataUsecase.execute(command);
+  //   }
+
+  @Get('/ping')
+  async x() {
+    return 'test';
   }
 }
